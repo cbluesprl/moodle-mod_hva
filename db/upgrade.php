06b7277d2fd31e8593c4ebc7e42b41f2be063324
Hannecart Loïc <lhannecart@cblue.be>
2022-08-12 14:57:15 +0200

    CB-399689 add upgrade file

---
+++ db/upgrade.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/**
+ * @package     mod_hva
+ * @author      Loïc Hannecart <lhannecart@cblue.be>
+ * @copyright   2022 CBlue (https://www.cblue.be/)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * @param $oldversion
+ * @return bool
+ * @throws ddl_exception
+ * @throws ddl_field_missing_exception
+ * @throws ddl_table_missing_exception
+ */
+function xmldb_hva_upgrade($oldversion)
+{
+    return true;
+}
